27fa5a85a6dc223cb9684b3a9bd2f4102f5344e8
Anro18 <131651425+Anro18@users.noreply.github.com>
2023-04-24 11:01:10 +0300

    Add files via upload

---
+++ задач1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "russian");
+	string cities[10] = { "Moscow", "New York", "Amsterdam", "Paris", "Berlin", "Athens", "Beijing", "Ankara", "Sydney", "Almaty" };
+	string city1, city2;
+	bool flag1 = false, flag2 = false;
+	for (int i = 0; i < 10; i++)
+	{
+		if (cities[i][0] == 'A')
+		{
+			if (!flag1) 
+			{
+				city1 = cities[i];
+				flag1 = true;
+			}
+			else {
+				city2 = cities[i];
+				flag2 = true;
+				break;
+			}
+		}
+	}
+	if (flag1 && flag2) 
+	{
+		swap(city1, city2);
+		cout << "Поменянные местами города:" << city1 << ", " << city2 << endl;
+	}
+	return 0;
+}
+
